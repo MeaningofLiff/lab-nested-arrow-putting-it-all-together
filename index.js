63ba180381b09e1c0a4ca0151e0a0cb57d4f63ff
Leah <leah.beder@gmail.com>
2025-08-05 20:04:26 -0500

    JavaScript Functions

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  let locked = false;
 
+  return (passwordAttempt) => {
+    if (locked) {
+      return "Account locked due to too many failed login attempts";
+    }
 
+    attemptCount++;
+
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    }
+
+    // Return the failure message for first 3 attempts
+    const message = `Attempt ${attemptCount}: Login failed`;
+
+    // Lock after the 3rd failed attempt
+    if (attemptCount >= 3) {
+      locked = true;
+    }
+
+    return message;
+  };
+}
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  createLoginTracker
 };
